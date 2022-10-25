8e8e9f05de23894a1d2d1815df47222df17c7352
Sara Språng <sara.sprang@epidemicsound.com>
2022-10-25 11:14:21 +0200

    Added paged person query.

--- src/main/kotlin/com/example/smallgraphqlapi/database/PersonEntityRepository.kt
+++ src/main/kotlin/com/example/smallgraphqlapi/database/PersonEntityRepository.kt
@@ -1,7 +1,8 @@
 package com.example.smallgraphqlapi.database
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface PersonEntityRepository : CrudRepository<PersonEntity, Int>
+interface PersonEntityRepository : CrudRepository<PersonEntity, Int>, PagingAndSortingRepository<PersonEntity, Int>
--- src/main/kotlin/com/example/smallgraphqlapi/graphql/PersonController.kt
+++ src/main/kotlin/com/example/smallgraphqlapi/graphql/PersonController.kt
@@ -2,6 +2,9 @@ package com.example.smallgraphqlapi.graphql
 
 import com.example.smallgraphqlapi.database.PersonEntity
 import com.example.smallgraphqlapi.database.PersonEntityRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
@@ -14,6 +17,11 @@ class PersonController(private val personRepository: PersonEntityRepository) {
     @QueryMapping
     fun findPerson(@Argument id: Int): PersonEntity? {
         return personRepository.findByIdOrNull(id)
+    }
+
+    @QueryMapping
+    fun findPersons(@Argument page: Int, @Argument size: Int): Page<PersonEntity> {
+        return personRepository.findAll(PageRequest.of(page, size))
     }
 
     @SchemaMapping(typeName = "Person")
